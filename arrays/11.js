70ed2cd9f33de42ffa5a6c5a29d6880536e1283c
mrfurm4n <mr.furmand@gmail.com>
2021-11-27 11:01:28 +0200

    make arrays-11

---
+++ arrays/11.js
@@ -0,0 +1,13 @@
+const buildDefinitionList = (array) => {
+	if (array.length === 0) return '';
+
+	const result = [];
+	for (const item of array) {
+		result.push(`<dt>${item[0]}</dt>`);
+		result.push(`<dd>${item[1]}</dd>`);
+	}
+
+	return `<dl>${result.join('')}</dl>`;
+}
+
+export default buildDefinitionList;
