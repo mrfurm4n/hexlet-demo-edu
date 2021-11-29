d7d806b8901fca622b30d32d573b66a3b511ff6a
mrfurm4n <mr.furmand@gmail.com>
2021-11-29 13:53:26 +0200

    refactor objects-12

--- objects/12.js
+++ objects/12.js
@@ -1,31 +1,33 @@
 import crc32 from 'crc-32';
 
-const getIndex = (key) => {
-	const hash = crc32.str(key);
-	return Math.abs(hash) % 1000;
+const getIndex = (key) => Math.abs(crc32.str(key)) % 1000;
+
+const make = () => [];
+
+const hasCollision = (map, key) => {
+	const index = getIndex(key);
+	const [currentKey] = map[index];
+	return currentKey !== key;
 };
 
-export const make = () => [];
-
-export const set = (map, key, value) => {
+const set = (map, key, value) => {
 	const index = getIndex(key);
-
-	if (Array.isArray(map[index])) {
-		const preventKey = map[index][0] ? map[index][0] : '';
-		if (preventKey !== key) return false;
+	if (map[index] && hasCollision(map, key)) {
+		return false;
 	}
 
 	map[index] = [key, value];
 	return true;
 };
 
-export const get = (map, key, defaultValue = null) => {
+const get = (map, key, defaultValue = null) => {
 	const index = getIndex(key);
-
-	if (!Array.isArray(map[index])) return defaultValue;
-
-	const preventKey = map[index][0];
-	if (preventKey !== key) return defaultValue;
-
-	return map[index][1];
+	if (!map[index] || hasCollision(map, key)) {
+		return defaultValue;
+	}
+	
+	const [, value] = map[index];
+	return value;
 };
+
+export { make, set, get };
